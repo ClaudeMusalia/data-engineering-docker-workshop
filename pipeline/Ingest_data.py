e1911761ba949cc2339b08565a94b9f4ad314a91
Claude Musalia <106872356+ClaudeMusalia@users.noreply.github.com>
2026-01-26 17:37:40 +0000

    pipeline code

---
+++ pipeline/Ingest_data.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+
+# Specify the data types for each column
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+def run():
+    pg_user = "root"
+    pg_pass = "root"
+    pg_host = "localhost"
+    pg_port = 5432
+    pg_db = "ny_taxi"
+
+    year = 2021
+    month = 1
+
+    target_table = "yellow_taxi_data"
+
+    chunksize = 100000
+
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+#Ingesting data in chunks since we cannot take the whole dataset at once. We will do batches and use an iterator for that. 
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+            # Create table schema (no data)
+            df_chunk.head(0).to_sql(
+                name="target_table",
+                con=engine,
+                if_exists="replace"
+            )
+            first = False
+            print("Table created")
+
+        # Insert chunk
+        df_chunk.to_sql(
+            name="target_table",
+            con=engine,
+            if_exists="append"
+        )
+
+if __name__ == "__main__":
+    run()
+
+
+
+
+
